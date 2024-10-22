900d64007eca66ecfc7f71e92aaed85a7a3fdd9f
Salim <58941116+salim-joshua@users.noreply.github.com>
2024-10-22 22:14:04 +0200

    Added selectors in ts for every major element & Created input check function for the form element.

--- dest/script.js
+++ dest/script.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("Hello!");
+const list = document.querySelector("#list");
+const form = document.getElementById("#new-task-form");
+const input = document.querySelector("'new-task-title");
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", e => {
+    e.preventDefault();
+    if ((input === null || input === void 0 ? void 0 : input.value) == "" || (input === null || input === void 0 ? void 0 : input.value) == null)
+        return;
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,9 @@
-console.log("Hello!");
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("#new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("'new-task-title")
+
+form?.addEventListener("submit", e => {
+    e.preventDefault()
+
+    if (input?.value == "" || input?.value == null) return;
+})
