4e4918eb35f053ae6b1c3a5c885999d9a80ff78e
Salim <58941116+salim-joshua@users.noreply.github.com>
2024-10-23 15:21:04 +0200

    Added an uniqe ID generator for being able to creating UIDs for each Note

---
+++ src/idGenerator.ts
@@ -0,0 +1,24 @@
+const generatedIds = new Set<string>();
+
+function generateRandomId(): string {
+
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let uniqueId = '';
+
+    do {
+        uniqueId = "";
+        for (let i = 0; i < 20; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            if (i % 5 === 0 && i !== 0) {
+                uniqueId += "-" + characters[randomIndex];
+            } else {
+                uniqueId += characters[randomIndex];
+            }
+        }
+    } while (generatedIds.has(uniqueId));
+
+
+    return uniqueId;
+}
+
+console.log(generateRandomId());
